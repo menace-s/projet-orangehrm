cc506d0ce2ade5541eaf1fc192bcf16b2eeb6136
menace-s <akolajeanphilippe@gmail.com>
2026-02-10 10:36:52 +0100

    FIX : mise a jour du timeout dans le config

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -5,13 +5,16 @@ export class LoginPage {
         // Sélecteurs (locators) - à compléter
         this.usernameInput = page.getByRole("textbox", { name: "Username" });
         this.passwordInput = page.getByRole("textbox", { name: "Password" });
+        // this.usernameInput = page.getByPlaceholder('Username');
+        // this.passwordInput = page.getByPlaceholder('Password');
         this.loginButton = page.getByRole("button", { name: "Login" });
     }
 
     // Actions - à compléter
     async goto() {
         // Navigation vers la page de login
-        await this.page.goto('/web/index.php/auth/login')
+        await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        // await this.page.goto('https://mon-portfolio-aganh-kola.vercel.app/');
     }
 
     async login(username, password) {
